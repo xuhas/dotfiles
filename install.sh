176d102fd79038da590ef65f8e92db9ef6366547
Xhulio Hasani <xhulioh@gmail.com>
2022-01-09 15:20:14 -0500

    spacebar

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@
 ln -s $PWD/yabai/.yabairc ~/.yabairc
 ln -s $PWD/skhd/.skhdrc ~/.skhdrc
 ln -s $PWD/kitty/kitty.conf ~/.config/kitty/kitty.conf
-
+ln -s $PWD/spacebar/spacebarrc ~/.config/spacebar/spacebarrc
